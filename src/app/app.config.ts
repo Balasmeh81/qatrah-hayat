6fc9c913639782159ece0c69fefd7b4ffa34f1f9
Abdullah-Balasmeh <abdullahbalasmeh.aa@gmail.com>
2026-03-02 21:29:39 +0300

    added footer and languge services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled' })
+    ),
+    provideTranslateService({
+      fallbackLang: 'en'
+    }),
+    provideTranslateHttpLoader({
+      prefix: './assets/i18n/',
+      suffix: '.json'
+    })]
 };
--- src/app/layouts/public-layout/public-layout.component.ts
+++ src/app/layouts/public-layout/public-layout.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { LandingHeaderComponent } from "../../features/landing/components/landing-header/landing-header.component";
+import { FooterComponent } from "../../shared/components/footer/footer.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-public-layout',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, LandingHeaderComponent, FooterComponent],
   templateUrl: './public-layout.component.html',
   styleUrl: './public-layout.component.css'
 })
