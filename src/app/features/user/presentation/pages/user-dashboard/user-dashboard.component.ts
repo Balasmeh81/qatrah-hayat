6a2ad7389283e5f298227d56b724fafcc37d2b15
Abdullah-Balasmeh <abdullahbalasmeh.aa@gmail.com>
2026-04-10 19:36:37 +0300

    UrgentBloodNeedsComponent ui done in dashboard

--- src/app/features/user/presentation/pages/user-dashboard/user-dashboard.component.ts
+++ src/app/features/user/presentation/pages/user-dashboard/user-dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UserHeroComponent } from "../../components/user-hero/user-hero.component";
 import { StatusCardDashboardComponent } from "../../components/status-card-dashboard/status-card-dashboard.component";
+import { UrgentBloodNeedsComponent } from "../../components/urgent-blood-needs/urgent-blood-needs.component";
 
 @Component({
   selector: 'app-user-dashboard',
-  imports: [UserHeroComponent, StatusCardDashboardComponent],
+  imports: [UserHeroComponent, StatusCardDashboardComponent, UrgentBloodNeedsComponent],
   templateUrl: './user-dashboard.component.html',
   styleUrl: './user-dashboard.component.css'
 })
