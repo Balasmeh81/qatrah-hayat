f6484d00e1d23aef105d50e928c53b0a02c5907e
Abdullah-Balasmeh <abdullahbalasmeh.aa@gmail.com>
2026-04-10 19:48:31 +0300

    dashbord ui almost done

--- src/app/features/user/presentation/pages/user-dashboard/user-dashboard.component.ts
+++ src/app/features/user/presentation/pages/user-dashboard/user-dashboard.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { UserHeroComponent } from "../../components/user-hero/user-hero.component";
 import { StatusCardDashboardComponent } from "../../components/status-card-dashboard/status-card-dashboard.component";
 import { UrgentBloodNeedsComponent } from "../../components/urgent-blood-needs/urgent-blood-needs.component";
+import { CampaignsSectionComponent } from '../../../../landing/presentation/components/campaigns-section/campaigns-section.component';
 
 @Component({
   selector: 'app-user-dashboard',
-  imports: [UserHeroComponent, StatusCardDashboardComponent, UrgentBloodNeedsComponent],
+  imports: [UserHeroComponent, StatusCardDashboardComponent, UrgentBloodNeedsComponent, CampaignsSectionComponent],
   templateUrl: './user-dashboard.component.html',
   styleUrl: './user-dashboard.component.css'
 })
