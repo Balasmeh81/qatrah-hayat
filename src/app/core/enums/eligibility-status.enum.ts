53fbe8fe4ffae5c707f23ad937c5621340a8c2dc
Abdullah-Balasmeh <abdullahbalasmeh.aa@gmail.com>
2026-04-10 18:51:32 +0300

    StatusCardDashboardComponent ui done

--- src/app/core/enums/eligibility-status.enum.ts
+++ src/app/core/enums/eligibility-status.enum.ts
@@ -3,3 +3,9 @@ export enum EligibilityStatus {
   TempDeferred = 2,
   PermDeferred = 3
 }
+
+export const EligibilityStatus_OPTIONS = [
+  { value: EligibilityStatus.Eligible, label: 'EligibilityStatusOpts.Eligible' },
+  { value: EligibilityStatus.TempDeferred, label: 'EligibilityStatusOpts.TempDeferred' },
+  { value: EligibilityStatus.PermDeferred, label: 'EligibilityStatusOpts.PermDeferred' }
+];
